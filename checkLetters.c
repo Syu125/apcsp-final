263d2abd3a3b70eb3230e13792bff08674c870f3
Sophia Yu <yusophia2011@gmail.com>
2019-06-03 09:11:13 -0700

    checkLetters

--- checkLetters.c
+++ checkLetters.c
@@ -1,2 +1,9 @@
-checkletters
-
+#include <stdio.h>
+char[][] checkLetters(char input, char word[][], int index, char result[]){
+for(int i = 0; i < 6; i++){
+	if(input == word[index][i]){
+	result[i] = input;
+	}
+}
+return result;
+}
