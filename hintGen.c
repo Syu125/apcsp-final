6e224b12ff8fa1625a20d1087bf5803d3127b35e
Sophia Yu <yusophia2011@gmail.com>
2019-06-04 09:28:18 -0700

    HintGen

--- hintGen.c
+++ hintGen.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-char [] hintGen(char c[][], int index, char ansC[]){
-int r = rand %4;
+char * hintGen(char c[][7], int index, char ansC[]){
+int r = rand() %2;
 for(int i = 0; i < 6; i++){
-if(r == 1){
+//printf("#: %d\n",r);
+//printf("%c",c[index][i]);
+if(r == 0){
 ansC[i] = c[index][i];
+printf("%c %c\n",ansC[i],c[index][i]);
+r = rand() % 2;
+}else{
+r = rand() % 2;
 }
 }
 return ansC;
+}
+
+int main(){
+char c[][7] = {
+	"ahsdgs",
+	"sasdge"
+    };
+
+int i = rand() % 2;
+//printf("%d",i);
+char ans[] = "_ _ _ _ _ _";
+char r[7];
+r = hintGen(c,i,ans);
+
+/*for(int i = 0; i < 3; i++){
+	printf("%c", re[i]);
+	}*/
 }
