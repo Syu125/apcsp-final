a3801bb79a5628923f31597d70e8646545ad7eeb
Syu125 <yusophia2011@gmail.com>
2019-06-04 16:37:18 -0700

    HintGen_complete(?)

--- hintGen.c
+++ hintGen.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-char * hintGen(char c[][7], int index, char ansC[]){
-int r = rand() %2;
-for(int i = 0; i < 6; i++){
-//printf("#: %d\n",r);
-//printf("%c",c[index][i]);
-if(r == 0){
-ansC[i] = c[index][i];
-printf("%c %c\n",ansC[i],c[index][i]);
-r = rand() % 2;
-}else{
-r = rand() % 2;
-}
+#include <string.h>
+#include <time.h>
+int rand (void);
+
+char * hintGen (char c[][7], int index, char ansC[]){
+int r = rand () % 3;
+  
+for (int i = 0; i < 6; i++){
+	//printf("#: %d\n",r);
+	//printf("%c",c[index][i]);
+	if (r == 0){
+        ansC[i] = c[index][i];
+        //printf ("%c %cn", ansC[i], c[index][i]);
+        r = rand () % 3;
+	}else{
+	    r = rand() % 3;
+	}
 }
 return ansC;
 }
 
-int main(){
-char c[][7] = {
-	"ahsdgs",
-	"sasdge"
-    };
+int main (){
+srand(time(0));
 
+char c[][7] = { 
+"ahsdgs", 
+"sasdge" 
+  };
+  
 int i = rand() % 2;
 //printf("%d",i);
-char ans[] = "_ _ _ _ _ _";
+char ans[] = "______";
 char r[7];
-r = hintGen(c,i,ans);
-
-/*for(int i = 0; i < 3; i++){
-	printf("%c", re[i]);
-	}*/
+strcpy (r, hintGen (c, i, ans));
+for(int i = 0; i < 6; i++){
+	printf("%c", r[i]);
+	} 
 }
