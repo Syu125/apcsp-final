0a61667dddb56f7494f462844105473b7959462f
Syu125 <yusophia2011@gmail.com>
2019-06-04 17:08:14 -0700

    CheckLetters_complete(?)

--- checkLetters.c
+++ checkLetters.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
-char[][] checkLetters(char input, char word[][], int index, char result[]){
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+char * checkLetters(char input, char word[][7], int index, char ans[]){
 for(int i = 0; i < 6; i++){
-	if(input == word[index][i]){
-	result[i] = input;
+	if(input==word[index][i]){
+	ans[i] = word[index][i];
 	}
 }
-return result;
+return ans;
+}
+
+int main (){
+srand(time(0));
+
+char c[][7] = { 
+"ahsdgs", 
+"sasdga" 
+  };
+  
+int i = rand() % 2;
+//printf("%d",i);
+char ans[] = "______";
+char r[7];
+char in = 'a';
+strcpy (r, checkLetters(in, c, 1, ans));
+for(int i = 0; i < 6; i++){
+	printf("%c", r[i]);
+	} 
 }
